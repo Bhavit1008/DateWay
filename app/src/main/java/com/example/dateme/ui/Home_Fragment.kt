fbf087601c83b2c57de67efee46d44dfc182c457
yashmal99 <malasiyayash99@gmail.com>
2021-05-13 21:09:29 +0530

    swipe implemented

--- app/src/main/java/com/example/dateme/ui/Home_Fragment.kt
+++ app/src/main/java/com/example/dateme/ui/Home_Fragment.kt
@@ -9,9 +9,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.dateme.R
+import com.example.dateme.ui.auth.OnSwipeTouchListener
 import com.thecode.aestheticdialogs.*
 import nl.dionsegijn.konfetti.KonfettiView
 
@@ -42,6 +45,10 @@ class Home_Fragment : Fragment() {
         // Inflate the layout for this fragment
         val view: View = inflater!!.inflate(R.layout.fragment_home_, container, false)
         val like_btn: ImageView = view.findViewById(R.id.like_btn)
+        val swiper_image: ImageView = view.findViewById(R.id.img_swiper)
+        val swiper_name: TextView = view.findViewById(R.id.txt_name_swiper)
+        val swiper_age: TextView= view.findViewById(R.id.txt_age_swiper)
+        val swiper_location: TextView = view.findViewById(R.id.txt_location_swiper)
 
 
        like_btn.setOnClickListener {
@@ -63,6 +70,40 @@ class Home_Fragment : Fragment() {
            like_btn.setImageResource(R.drawable.ic_baseline_favorite_24_colored)
        }
 
+        activity?.applicationContext?.let {
+            view.setOnTouchListener(object : OnSwipeTouchListener(it) {
+
+                override fun onSwipeTop() {
+                    super.onSwipeTop()
+                    Toast.makeText(activity,"swiped top",Toast.LENGTH_SHORT).show()
+                    swiper_image.setImageResource(R.drawable.rashmika_mandanna)
+                    swiper_name.text = getString(R.string.name)
+                    swiper_age.text = getString(R.string.age)
+                    swiper_location.text = getString(R.string.location)
+
+                }
+                override fun onSwipeBottom() {
+                    super.onSwipeBottom()
+                    Toast.makeText(activity,"swiped bottom",Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onClick() {
+                    super.onSwipeRight()
+                    Toast.makeText(activity,"swiped clicked",Toast.LENGTH_SHORT).show()
+                }
+                override fun onDoubleClick() {
+                    super.onSwipeRight()
+                    Toast.makeText(activity,"swiped double clicked",Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onLongClick() {
+                    super.onSwipeRight()
+                    Toast.makeText(activity,"swiped long click",Toast.LENGTH_SHORT).show()
+                }
+
+
+            })
+        }
         // Return the fragment view/layout
         return view
     }
